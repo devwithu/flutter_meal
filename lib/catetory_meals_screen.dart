9be475eecb4eacb1e701f959430b535ee5ec32b9
devwithu <devwithu@naver.com>
2020-01-09 08:24:01 +0900

    ch 8

--- lib/catetory_meals_screen.dart
+++ lib/catetory_meals_screen.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
+  final String categoryId;
+  final String catetoryTitle;
+
+  CategoryMealsScreen(this.categoryId, this.catetoryTitle);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('The Recipes'),
+        title: Text(catetoryTitle),
       ),
       body: Center(
         child: Text('The Recipes For The Category!'),
