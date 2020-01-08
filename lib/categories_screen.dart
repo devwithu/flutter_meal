7cefbbefee85376ef9a897bcfa43a2ff7ee010f0
devwithu <devwithu@naver.com>
2020-01-09 07:34:38 +0900

    init

---
+++ lib/categories_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'dummy_data.dart';
+import './catogory_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('DeliMeal'),),
+      body: GridView(
+      children: DUMMY_CATEGORIES
+          .map((catData) => CategoryItem(catData.title, catData.color))
+          .toList(),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 1.5,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20),
+    ),
+    );
+  }
+}
