74927fdee08d6e4391eb92ff62e1ac88ccdb580c
devwithu <devwithu@naver.com>
2020-01-10 11:35:29 +0900

    ch 10

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_meal/screens/meal_detail_screen.dart';
-import './screens/catetory_meals_screen.dart';
+
+import './screens/tabs_screen.dart';
+import './screens/meal_detail_screen.dart';
+import './screens/category_meals_screen.dart';
 import './screens/categories_screen.dart';
 
 void main() => runApp(MyApp());
@@ -16,55 +18,37 @@ class MyApp extends StatelessWidget {
         canvasColor: Color.fromRGBO(255, 254, 229, 1),
         fontFamily: 'Raleway',
         textTheme: ThemeData.light().textTheme.copyWith(
-          body1: TextStyle(
-            color: Color.fromRGBO(20, 51, 51, 1),
+            body1: TextStyle(
+              color: Color.fromRGBO(20, 51, 51, 1),
             ),
-          body2: TextStyle(
-            color: Color.fromRGBO(20, 51, 51, 1),
-          ),
-          title: TextStyle(
-            fontSize: 20,
-            fontFamily: 'RobotoCondensed',
-            fontWeight: FontWeight.bold,
-          )
-        )
+            body2: TextStyle(
+              color: Color.fromRGBO(20, 51, 51, 1),
+            ),
+            title: TextStyle(
+              fontSize: 20,
+              fontFamily: 'RobotoCondensed',
+              fontWeight: FontWeight.bold,
+            )),
       ),
-      //home: CategoriesScreen(),
-      initialRoute: '/ ',
+      // home: CategoriesScreen(),
+      initialRoute: '/', // default is '/'
       routes: {
-        '/': (ctx) => CategoriesScreen(),
+        '/': (ctx) => TabsScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
       },
       onGenerateRoute: (settings) {
         print(settings.arguments);
-        return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
+        // if (settings.name == '/meal-detail') {
+        //   return ...;
+        // } else if (settings.name == '/something-else') {
+        //   return ...;
+        // }
+        // return MaterialPageRoute(builder: (ctx) => CategoriesScreen(),);
       },
       onUnknownRoute: (settings) {
-         return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen(),);
       },
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('DeliMeals'),
-      ),
-      body: Center(
-        child: Text('Navigation Time!'),
-      ),
     );
   }
 }
---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_meal/screens/categories_screen.dart';
+import 'package:flutter_meal/screens/favorites_screen.dart';
+
+class TabsScreen extends StatefulWidget {
+  @override
+  _TabScreenState createState() => _TabScreenState();
+}
+
+class _TabScreenState extends State<TabsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      //initialIndex: 0,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Meals'),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(
+                  Icons.category
+                ),
+                text: 'Categories',
+              ),
+              Tab(
+                icon: Icon(
+                  Icons.star
+                ),
+                text: 'Favorites',
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            CategoriesScreen(), 
+            FavoritesScreen()
+          ],
+        ),
+      ),
+    );
+  }
+}
